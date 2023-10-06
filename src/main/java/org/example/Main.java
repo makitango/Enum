1f0bc597d530a64fba471be764a78b2b81d6dec4
Dominic Bergmann <dominic.bergmann@gmail.com>
2023-10-06 11:40:44 +0200

    Refactor main functions into Main file

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,19 +1,19 @@
 package org.example;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import static org.example.DaysOfWeekUtil.getDayType;
+
 public class Main {
     public static void main(String[] args) {
-        // Press Opt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        //getDayType
+        DaysOfWeek monday = DaysOfWeek.MONDAY;
+        DaysOfWeek saturday = DaysOfWeek.SATURDAY;
+        System.out.println(monday + " ist ein " + getDayType(monday));
+        System.out.println(saturday + " ist ein " + getDayType(saturday));
 
-        // Press Ctrl+R or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Ctrl+D to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Cmd+F8.
-            System.out.println("i = " + i);
-        }
+        //favouriteDay
+        Person person1 = new Person(1, "Jamie Lannister", DaysOfWeek.MONDAY);
+        Person person2 = new Person(2, "Arthur Dayne", DaysOfWeek.FRIDAY);
+        System.out.println("Person 1: " + person1.name() + " bevorzugt " + person1.favoriteDay());
+        System.out.println("Person 2: " + person2.name() + " bevorzugt " + person2.favoriteDay());
     }
 }
