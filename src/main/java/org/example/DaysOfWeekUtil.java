09e270e988541b12e6e18f2de09afaa6daa2bc82
Dominic Bergmann <dominic.bergmann@gmail.com>
2023-10-06 11:28:55 +0200

    Adjust DaysOfWeekUtil to incorporate correct weekday naming

--- src/main/java/org/example/DaysOfWeekUtil.java
+++ src/main/java/org/example/DaysOfWeekUtil.java
@@ -4,11 +4,12 @@ import java.util.Optional;
 
 public class DaysOfWeekUtil {
     public static String getDayType(DaysOfWeek day) {
-        String dayType = (day == DaysOfWeek.MONDAY || day == DaysOfWeek.TUESDAY ||
-                day == DaysOfWeek.WEDNESDAY || day == DaysOfWeek.THURSDAY ||
-                day == DaysOfWeek.FRIDAY) ? "Wochentag" :
-                ((day == DaysOfWeek.SATURDAY || day == DaysOfWeek.SUNDAY) ? "Wochenendtag" : "Ungültiger Wochentag");
-        return dayType;
+        return Optional.of(day)
+                .filter(d -> d == DaysOfWeek.MONDAY || d == DaysOfWeek.TUESDAY ||
+                        d == DaysOfWeek.WEDNESDAY || d == DaysOfWeek.THURSDAY ||
+                        d == DaysOfWeek.FRIDAY)
+                .map(DaysOfWeek::toString)
+                .orElse(day == DaysOfWeek.SATURDAY || day == DaysOfWeek.SUNDAY ? "Wochenende" : "Ungültiger Wochentag");
     }
 
     public static void main(String[] args) {
